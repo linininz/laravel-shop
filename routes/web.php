87f92960be35db382b69fa9c9e04dcf524481aaf
Lininin <15602239711lzh@gmail.com>
2021-03-21 20:14:42 +0800

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::redirect('/', '/products')->name('root');
-Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
-
 Auth::routes(['verify' => true]);
-
+Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
@@ -31,3 +27,5 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 });
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
